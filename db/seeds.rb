05fc603c2ae1a1b25128fecf82ad001f3d429d53
bran31 <bran31@gmail.com>
2019-01-02 01:51:37 -0500

    added migration for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "James", password: "password")
+User.create(username: "Robin", password: "bastard7")
+User.create(username: "Landon", password: "maaaaaa")
